783a3879eb463241f04229e122f6a671a65af3b0
Arnold Flores <arnold.flores.v@gmail.com>
2024-07-15 18:24:39 -0600

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO course (id, name) VALUES (1, 'Java'), (2, 'Spring Boot'), (3, 'React');
 INSERT INTO status (id, name) VALUES (1, 'OPEN'), (2, 'CLOSED');
 INSERT INTO profile (id, name) VALUES (1, 'USER'), (2, 'ADMIN');
-INSERT INTO user (id, name, email, password, profile_id) VALUES (1, 'John Doe', 'john@example.com', 'password', 1);
+INSERT INTO user (id, name, email, password, profile_id) VALUES (1, 'Juan jose', 'juan.jose@forohub.com', 'password', 1);
